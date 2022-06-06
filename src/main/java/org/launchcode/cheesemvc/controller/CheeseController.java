73591b81ea20bf4412c63b59ad3f67bda0f6fe22
harsh <harsh2001apcr@gmail.com>
2022-06-07 00:54:42 +0530

    thymeleaf fragments, default-template, link-to-css/js files using th:src/th:href tags view (3)

--- src/main/java/org/launchcode/cheesemvc/controller/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controller/CheeseController.java
@@ -30,7 +30,8 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
-    public String displayAddCheeseForm() {
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
         return "cheese/add";
     }
 
